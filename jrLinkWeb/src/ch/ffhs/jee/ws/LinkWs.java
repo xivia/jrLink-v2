4f9b1437fb0e231ecc6375ac14eeb11063b3175b
Roman Born <roman.born@xivia.ch>
2014-11-20 19:55:57 +0100

    add query params

--- jrLinkWeb/src/ch/ffhs/jee/ws/LinkWs.java
+++ jrLinkWeb/src/ch/ffhs/jee/ws/LinkWs.java
@@ -8,11 +8,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import ch.ffhs.jee.util.ErrorWs;
-
 import ch.ffhs.jee.controller.LinkBeanLocal;
 import ch.ffhs.jee.model.Link;
 
@@ -27,7 +27,12 @@ public class LinkWs {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAll() {
+	public Response getAll( @QueryParam(value = "u") String user, 
+							@QueryParam(value = "p") String password) {
+		
+		// check user, password
+		
+		
 		Collection<Link> links = linkBean.getList();
 		
 		return Response.status(200).entity(links).build();
@@ -36,7 +41,9 @@ public class LinkWs {
 	@GET
 	@Path("{id}/")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getById(@PathParam("id") String id) {
+	public Response getById(@PathParam("id") String id, 
+							@QueryParam(value = "u") String user, 
+							@QueryParam(value = "p") String password) {
 		Response response;
 		
 		Long lid;
