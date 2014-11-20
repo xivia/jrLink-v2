1e255cb1ad53fadcc1f850e126a56824fc256cb7
Roman Born <roman.born@xivia.ch>
2014-11-20 16:20:24 +0100

    add Link-Bean

---
+++ jrLinkEJB/ejbModule/ch/ffhs/jee/controller/LinkBean.java
@@ -0,0 +1,67 @@
+package ch.ffhs.jee.controller;
+
+import java.util.Collection;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import ch.ffhs.jee.model.Link;
+
+/**
+ * Session Bean implementation class LinkBean
+ */
+@Stateless
+public class LinkBean implements LinkBeanLocal {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+    /**
+     * Default constructor. 
+     */
+    public LinkBean() { }
+
+	@Override
+	public Collection<Link> getList() {
+		return em.createNamedQuery("Link.findAll",Link.class).getResultList();
+	}
+
+	@Override
+	public Link getById(Long id) {
+		return em.find(Link.class, new Long(id));
+	}
+
+	@Override
+	public void create(String name, String url) {
+		Link link = new Link();
+		
+		link.setName(name);
+		link.setUrl(url);
+		
+		em.persist(link);
+		em.flush();
+	}
+
+	@Override
+	public void update(Long id, String name, String url) {
+		Link link = getById(id);
+		
+		link.setName(name);
+		link.setUrl(url);
+		
+		em.merge(link);
+		em.flush();
+	}
+
+	@Override
+	public void delete(Long id) {
+		Link link = getById(id);
+		
+		if (link != null) em.remove(link);
+		em.flush();
+	}
+    
+    
+
+}
---
+++ jrLinkEJB/ejbModule/ch/ffhs/jee/controller/LinkBeanLocal.java
@@ -0,0 +1,37 @@
+package ch.ffhs.jee.controller;
+
+import java.util.Collection;
+
+import javax.ejb.Local;
+
+import ch.ffhs.jee.model.Link;
+
+@Local
+public interface LinkBeanLocal {
+
+	/**
+	 * get all records
+	 */
+	Collection<Link> getList();	
+	
+	/**
+	 * get one record by id
+	 */
+	Link getById(Long id);
+
+	/**
+	 * add record
+	 */
+	void create(String name, String url);
+	
+	/**
+	 * update record
+	 */
+	void update(Long id, String name, String url);
+	
+	/**
+	 * delete record
+	 */
+	void delete(Long id);
+	
+}
