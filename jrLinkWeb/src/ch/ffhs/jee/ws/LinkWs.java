af1ccf45bd3652e76dc33589af6ca2bde0911906
Roman Born <roman.born@xivia.ch>
2014-11-21 15:49:25 +0100

    finish link ws

--- jrLinkWeb/src/ch/ffhs/jee/ws/LinkWs.java
+++ jrLinkWeb/src/ch/ffhs/jee/ws/LinkWs.java
@@ -4,7 +4,11 @@ import java.util.Collection;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -72,10 +76,10 @@ public class LinkWs {
 			
 			// check id (above catch) 
 			if (lid > 0) {
-				Link links = linkBean.getById(new Long(lid));
+				Link link = linkBean.getById(new Long(lid));
 				// check id (in database)
-				if (links != null) {
-					response = Response.status(200).entity(links).build();
+				if (link != null) {
+					response = Response.status(200).entity(link).build();
 				} else {
 					response = Response.status(404).entity(new ErrorWs404()).build();
 				}
@@ -89,7 +93,111 @@ public class LinkWs {
 		return response;
 	}
 	
+	@PUT
+	@Path("{id}/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response update(Link link, @PathParam("id") String id, 
+						   @QueryParam(value = "u") String user, 
+						   @QueryParam(value = "p") String password) {				
+		Response response;
+		
+		// check user, password
+		User luser = userBean.getByCredentials(user, password);
+		
+		if (luser != null) {
+			Long lid;
+			try {
+				lid = Long.parseLong(id); 
+			} 
+			catch (NumberFormatException e) {
+				lid = new Long(0);
+			}
+			
+			// check id (above catch) 
+			if (lid > 0) {
+				Link links = linkBean.getById(new Long(lid));
+				// check id (in database)
+				if (links != null) {
+					linkBean.update(lid, link.getName(), link.getUrl());
+					Link updLink = linkBean.getById(new Long(lid));
+					
+					response = Response.status(200).entity(updLink).build();
+				} else {
+					response = Response.status(404).entity(new ErrorWs404()).build();
+				}
+			} else {
+				response = Response.status(400).entity(new ErrorWs400()).build();
+			}			
+		} else {
+			response = Response.status(401).entity(new ErrorWs401()).build();
+		}
+
+		return response;
+	}
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response create(Link link,
+						   @QueryParam(value = "u") String user, 
+						   @QueryParam(value = "p") String password) {
+		Response response;
+		
+		// check user, password
+		User luser = userBean.getByCredentials(user, password);
+		
+		if (luser != null) {
+			Long lid = linkBean.create(link.getName(), link.getUrl());
+			Link newLink = linkBean.getById(new Long(lid));
+					
+			response = Response.status(200).entity(newLink).build();
+		} else {
+			response = Response.status(401).entity(new ErrorWs401()).build();
+		}
+
+		return response;
+	}
 	
+	@DELETE
+	@Path("{id}/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response delete(@PathParam("id") String id, 
+						   @QueryParam(value = "u") String user, 
+						   @QueryParam(value = "p") String password) {			
+		Response response;
+		
+		// check user, password
+		User luser = userBean.getByCredentials(user, password);
+		
+		if (luser != null) {
+			Long lid;
+			try {
+				lid = Long.parseLong(id); 
+			} 
+			catch (NumberFormatException e) {
+				lid = new Long(0);
+			}
+			
+			// check id (above catch) 
+			if (lid > 0) {
+				Link links = linkBean.getById(new Long(lid));
+				// check id (in database)
+				if (links != null) {
+					linkBean.delete(lid);
+					
+					response = Response.status(200).entity("").build();
+				} else {
+					response = Response.status(404).entity(new ErrorWs404()).build();
+				}
+			} else {
+				response = Response.status(400).entity(new ErrorWs400()).build();
+			}			
+		} else {
+			response = Response.status(401).entity(new ErrorWs401()).build();
+		}
+
+		return response;
+	}	
 	
 }
