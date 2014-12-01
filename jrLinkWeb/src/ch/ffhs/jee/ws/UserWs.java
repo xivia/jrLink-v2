420cf1261219b8aac53a71fd3b046869ef4b8507
Roman Born <roman.born@xivia.ch>
2014-12-01 11:21:33 +0100

    user ws - add put

--- jrLinkWeb/src/ch/ffhs/jee/ws/UserWs.java
+++ jrLinkWeb/src/ch/ffhs/jee/ws/UserWs.java
@@ -8,7 +8,9 @@ import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,19 +18,36 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import ch.ffhs.jee.controller.RoleBeanLocal;
 import ch.ffhs.jee.controller.UserBeanLocal;
 import ch.ffhs.jee.model.User;
 import ch.ffhs.jee.util.ErrorWs400;
 import ch.ffhs.jee.util.ErrorWs401;
 import ch.ffhs.jee.util.ErrorWs404;
+import ch.ffhs.jee.util.ErrorWs409;
 
 @Path("user")
 @Stateless
 public class UserWs {
 
+	// JSON data
+	public static class UserData {
+		private String name;
+		private String password;
+		private String active;
+		private String role;
+		
+		public String getName() { return name; }
+		public String getPassword() { return password; }
+		public Boolean isActive() { if (active != null) return new Boolean(active); else return null; }
+		public String getRole() { return role; }
+	}
+	
 	@EJB
 	private UserBeanLocal userBean;
-
+	@EJB
+	private RoleBeanLocal roleBean;
+	
 	public UserWs() { }
 	
 	@GET
@@ -93,9 +112,61 @@ public class UserWs {
 		return response;
 	}
 
-	// TODO put
+	@PUT
+	@Path("{id}/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response update(UserData uuser, @PathParam("id") String id, 
+						   @QueryParam(value = "u") String user, 
+						   @QueryParam(value = "p") String password) {				
+		Response response;
+		
+		// check user, password
+		User luser = userBean.getByCredentials(user, password);
+		
+		if ((luser != null) && (luser.isAdministrator())) {
+			Long lid;
+			try {
+				lid = Long.parseLong(id); 
+			} 
+			catch (NumberFormatException e) {
+				lid = new Long(0);
+			}
+			
+			// check id (above catch) 
+			if (lid > 0) {
+				User cuser = userBean.getById(new Long(lid));
+				// check id (in database)
+				if (cuser != null) {
+					Long result = userBean.update(lid, roleBean.getByName(uuser.getRole()), 
+													uuser.getName(), uuser.getPassword(), 
+													uuser.isActive());
+					
+					if (result > 0) {
+						User updUser = userBean.getById(new Long(lid));
+						
+						Collection<User> rusers = new ArrayList<User>();
+						rusers.add(updUser);
+						
+						response = Response.status(200).entity(formatJSON(rusers).iterator().next()).build();						
+					} else {
+						response = Response.status(409).entity(new ErrorWs409()).build();						
+					}
+				} else {
+					response = Response.status(404).entity(new ErrorWs404()).build();
+				}
+			} else {
+				response = Response.status(400).entity(new ErrorWs400()).build();
+			}
+		} else {
+			response = Response.status(401).entity(new ErrorWs401()).build();
+		}
+	
+		return response;
+	}
 	
 	// TODO post
+	// -> role not found?
 	
 	// TODO delete
 	
